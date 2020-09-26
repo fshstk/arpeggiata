dccb6e740dcedb52cce29b62c6a7cfd212b7ff73
fshstk <fshstk@fshstk.com>
2020-09-26 17:15:26 +0200

    Add basic flipcard functionality to synth panel.

--- src/components/Synth.vue
+++ src/components/Synth.vue
@@ -1,243 +1,273 @@
 <template>
-  <q-card class="synth-panel" dark flat bordered>
-    <q-card-section class="flex justify-between">
-      <div class="q-gutter-md">
-        <q-knob
-          v-model="synth.envelope.attack"
-          :min="0"
-          :max="0.1"
-          :step="0.001"
-          show-value
-          color="grey-3"
-          center-color="orange"
-          track-color="orange"
-          >A</q-knob
-        >
-        <q-knob
-          v-model="synth.envelope.decay"
-          :min="0"
-          :max="1"
-          :step="0.01"
-          show-value
-          color="grey-3"
-          center-color="orange"
-          track-color="orange"
-          >D</q-knob
-        >
-        <q-knob
-          v-model="synth.filterEnvelope.baseFrequency"
-          :min="100"
-          :max="10000"
-          :step="10"
-          show-value
-          color="grey-3"
-          center-color="orange"
-          track-color="orange"
-          >F</q-knob
-        >
-      </div>
-      <div class="text-h6">{{ name }}</div>
-      <div class="q-gutter-md">
-        <q-btn
-          @mousedown="powerButton()"
-          :class="isActive ? 'on' : 'off'"
-          :ripple="false"
-          round
-          icon="power_settings_new"
-        />
-      </div>
-    </q-card-section>
-
-    <q-separator inset dark />
-
-    <q-card-section>
-      <div class="row">
-        <div class="col-2">
-          <q-btn
-            label="1/4"
-            @mousedown="arpeggio.interval = 1 / 4"
-            :class="
-              arpeggio.interval === 1 / 4 ? 'note-length-active' : 'note-length'
-            "
-            :ripple="false"
-            class="third-button"
-            icon="music_note"
-            stack
-          />
-          <q-btn
-            label="1/8"
-            @mousedown="arpeggio.interval = 1 / 8"
-            :class="
-              arpeggio.interval === 1 / 8 ? 'note-length-active' : 'note-length'
-            "
-            :ripple="false"
-            class="third-button"
-            icon="music_note"
-            stack
-          />
-          <q-btn
-            label="1/16"
-            @mousedown="arpeggio.interval = 1 / 16"
-            :class="
-              arpeggio.interval === 1 / 16
-                ? 'note-length-active'
-                : 'note-length'
-            "
-            :ripple="false"
-            class="third-button"
-            icon="music_note"
-            stack
-          />
-        </div>
-
-        <div class="col-2">
-          <q-btn
-            label="1/4T"
-            @mousedown="arpeggio.interval = 1 / 3"
-            :class="
-              arpeggio.interval === 1 / 3 ? 'note-length-active' : 'note-length'
-            "
-            :ripple="false"
-            class="third-button"
-            icon="music_note"
-            stack
-          />
-          <q-btn
-            label="1/8T"
-            @mousedown="arpeggio.interval = 1 / 6"
-            :class="
-              arpeggio.interval === 1 / 6 ? 'note-length-active' : 'note-length'
-            "
-            :ripple="false"
-            class="third-button"
-            icon="music_note"
-            stack
-          />
-          <q-btn
-            label="1/16T"
-            @mousedown="arpeggio.interval = 1 / 12"
-            :class="
-              arpeggio.interval === 1 / 12
-                ? 'note-length-active'
-                : 'note-length'
-            "
-            :ripple="false"
-            class="third-button"
-            icon="music_note"
-            stack
-          />
-        </div>
-
-        <div class="col-3">
-          <q-btn
-            :label="$q.screen.gt.xs ? 'scale 1' : void 0"
-            @mousedown="changeScale(0)"
-            :class="scaleIndex === 0 ? 'sequence-active' : 'sequence'"
-            class="third-button"
-            :ripple="false"
-            icon="stairs"
-            stack
-          />
-          <q-btn
-            :label="$q.screen.gt.xs ? 'scale 2' : void 0"
-            @mousedown="changeScale(1)"
-            :class="scaleIndex === 1 ? 'sequence-active' : 'sequence'"
-            class="third-button"
-            :ripple="false"
-            icon="stairs"
-            stack
-          />
-          <q-btn
-            :label="$q.screen.gt.xs ? 'scale 3' : void 0"
-            @mousedown="changeScale(2)"
-            :class="scaleIndex === 2 ? 'sequence-active' : 'sequence'"
-            class="third-button"
-            :ripple="false"
-            icon="stairs"
-            stack
-          />
-        </div>
-
-        <div class="col-3">
-          <q-btn
-            :label="$q.screen.gt.xs ? 'seq 1' : void 0"
-            @mousedown="changeSequence(0)"
-            :class="sequenceIndex === 0 ? 'sequence-active' : 'sequence'"
-            class="third-button"
-            :ripple="false"
-            icon="loop"
-            stack
-          />
-          <q-btn
-            :label="$q.screen.gt.xs ? 'seq 2' : void 0"
-            @mousedown="changeSequence(1)"
-            :class="sequenceIndex === 1 ? 'sequence-active' : 'sequence'"
-            class="third-button"
-            :ripple="false"
-            icon="loop"
-            stack
-          />
-          <q-btn
-            :label="$q.screen.gt.xs ? 'seq 3' : void 0"
-            @mousedown="changeSequence(2)"
-            :class="sequenceIndex === 2 ? 'sequence-active' : 'sequence'"
-            class="third-button"
-            :ripple="false"
-            icon="loop"
-            stack
-          />
-        </div>
-
-        <div class="col-2">
-          <q-btn
-            icon="add"
-            @mousedown="octave = 1"
-            :class="octave === 1 ? 'octave-active' : 'octave'"
-            class="third-button"
-            :ripple="false"
-          />
-          <q-btn
-            icon="exposure_zero"
-            @mousedown="octave = 0"
-            :class="octave === 0 ? 'octave-active' : 'octave'"
-            class="third-button"
-            :ripple="false"
-          />
-          <q-btn
-            icon="remove"
-            @mousedown="octave = -1"
-            :class="octave === -1 ? 'octave-active' : 'octave'"
-            class="third-button"
-            :ripple="false"
-          />
-        </div>
-      </div>
-      <div class="row">
-        <q-list dense class="col-12">
-          <q-item>
-            <q-item-section side><q-icon name="volume_up"/></q-item-section>
-            <q-item-section>
-              <q-slider
-                v-model="synth.volume.value"
-                :min="-30"
-                :max="-6"
-                color="teal"
-              />
-            </q-item-section>
-            <q-item-section side>
+  <q-flashcard no-hover>
+    <div style="height: 325px; width: 500px; margin: 0; padding: 0;">
+      <q-flashcard-section transition="slide-up-out" :active="settingsOpen">
+        <q-card class="synth-panel-front" dark flat bordered>
+          <q-card-section class="flex justify-between">
+            <div class="q-gutter-md">
+              <q-knob
+                v-model="synth.envelope.attack"
+                :min="0"
+                :max="0.1"
+                :step="0.001"
+                show-value
+                color="grey-3"
+                center-color="orange"
+                track-color="orange"
+                >A</q-knob
+              >
+              <q-knob
+                v-model="synth.envelope.decay"
+                :min="0"
+                :max="1"
+                :step="0.01"
+                show-value
+                color="grey-3"
+                center-color="orange"
+                track-color="orange"
+                >D</q-knob
+              >
+              <q-knob
+                v-model="synth.filterEnvelope.baseFrequency"
+                :min="100"
+                :max="10000"
+                :step="10"
+                show-value
+                color="grey-3"
+                center-color="orange"
+                track-color="orange"
+                >F</q-knob
+              >
+            </div>
+            <div class="text-h6">{{ name }}</div>
+            <div class="q-gutter-md">
               <q-btn
+                @mousedown="powerButton()"
+                :class="isActive ? 'on' : 'off'"
                 :ripple="false"
                 round
-                icon="settings"
-                class="settings"
-                size="sm"
+                icon="power_settings_new"
               />
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </div>
-    </q-card-section>
-  </q-card>
+            </div>
+          </q-card-section>
+
+          <q-separator inset dark />
+
+          <q-card-section>
+            <div class="row">
+              <div class="col-2">
+                <q-btn
+                  label="1/4"
+                  @mousedown="arpeggio.interval = 1 / 4"
+                  :class="
+                    arpeggio.interval === 1 / 4
+                      ? 'note-length-active'
+                      : 'note-length'
+                  "
+                  :ripple="false"
+                  class="third-button"
+                  icon="music_note"
+                  stack
+                />
+                <q-btn
+                  label="1/8"
+                  @mousedown="arpeggio.interval = 1 / 8"
+                  :class="
+                    arpeggio.interval === 1 / 8
+                      ? 'note-length-active'
+                      : 'note-length'
+                  "
+                  :ripple="false"
+                  class="third-button"
+                  icon="music_note"
+                  stack
+                />
+                <q-btn
+                  label="1/16"
+                  @mousedown="arpeggio.interval = 1 / 16"
+                  :class="
+                    arpeggio.interval === 1 / 16
+                      ? 'note-length-active'
+                      : 'note-length'
+                  "
+                  :ripple="false"
+                  class="third-button"
+                  icon="music_note"
+                  stack
+                />
+              </div>
+
+              <div class="col-2">
+                <q-btn
+                  label="1/4T"
+                  @mousedown="arpeggio.interval = 1 / 3"
+                  :class="
+                    arpeggio.interval === 1 / 3
+                      ? 'note-length-active'
+                      : 'note-length'
+                  "
+                  :ripple="false"
+                  class="third-button"
+                  icon="music_note"
+                  stack
+                />
+                <q-btn
+                  label="1/8T"
+                  @mousedown="arpeggio.interval = 1 / 6"
+                  :class="
+                    arpeggio.interval === 1 / 6
+                      ? 'note-length-active'
+                      : 'note-length'
+                  "
+                  :ripple="false"
+                  class="third-button"
+                  icon="music_note"
+                  stack
+                />
+                <q-btn
+                  label="1/16T"
+                  @mousedown="arpeggio.interval = 1 / 12"
+                  :class="
+                    arpeggio.interval === 1 / 12
+                      ? 'note-length-active'
+                      : 'note-length'
+                  "
+                  :ripple="false"
+                  class="third-button"
+                  icon="music_note"
+                  stack
+                />
+              </div>
+
+              <div class="col-3">
+                <q-btn
+                  :label="$q.screen.gt.xs ? 'scale 1' : void 0"
+                  @mousedown="changeScale(0)"
+                  :class="scaleIndex === 0 ? 'sequence-active' : 'sequence'"
+                  class="third-button"
+                  :ripple="false"
+                  icon="stairs"
+                  stack
+                />
+                <q-btn
+                  :label="$q.screen.gt.xs ? 'scale 2' : void 0"
+                  @mousedown="changeScale(1)"
+                  :class="scaleIndex === 1 ? 'sequence-active' : 'sequence'"
+                  class="third-button"
+                  :ripple="false"
+                  icon="stairs"
+                  stack
+                />
+                <q-btn
+                  :label="$q.screen.gt.xs ? 'scale 3' : void 0"
+                  @mousedown="changeScale(2)"
+                  :class="scaleIndex === 2 ? 'sequence-active' : 'sequence'"
+                  class="third-button"
+                  :ripple="false"
+                  icon="stairs"
+                  stack
+                />
+              </div>
+
+              <div class="col-3">
+                <q-btn
+                  :label="$q.screen.gt.xs ? 'seq 1' : void 0"
+                  @mousedown="changeSequence(0)"
+                  :class="sequenceIndex === 0 ? 'sequence-active' : 'sequence'"
+                  class="third-button"
+                  :ripple="false"
+                  icon="loop"
+                  stack
+                />
+                <q-btn
+                  :label="$q.screen.gt.xs ? 'seq 2' : void 0"
+                  @mousedown="changeSequence(1)"
+                  :class="sequenceIndex === 1 ? 'sequence-active' : 'sequence'"
+                  class="third-button"
+                  :ripple="false"
+                  icon="loop"
+                  stack
+                />
+                <q-btn
+                  :label="$q.screen.gt.xs ? 'seq 3' : void 0"
+                  @mousedown="changeSequence(2)"
+                  :class="sequenceIndex === 2 ? 'sequence-active' : 'sequence'"
+                  class="third-button"
+                  :ripple="false"
+                  icon="loop"
+                  stack
+                />
+              </div>
+
+              <div class="col-2">
+                <q-btn
+                  icon="add"
+                  @mousedown="octave = 1"
+                  :class="octave === 1 ? 'octave-active' : 'octave'"
+                  class="third-button"
+                  :ripple="false"
+                />
+                <q-btn
+                  icon="exposure_zero"
+                  @mousedown="octave = 0"
+                  :class="octave === 0 ? 'octave-active' : 'octave'"
+                  class="third-button"
+                  :ripple="false"
+                />
+                <q-btn
+                  icon="remove"
+                  @mousedown="octave = -1"
+                  :class="octave === -1 ? 'octave-active' : 'octave'"
+                  class="third-button"
+                  :ripple="false"
+                />
+              </div>
+            </div>
+            <div class="row">
+              <q-list dense class="col-12">
+                <q-item>
+                  <q-item-section side
+                    ><q-icon name="volume_up"
+                  /></q-item-section>
+                  <q-item-section>
+                    <q-slider
+                      v-model="synth.volume.value"
+                      :min="-30"
+                      :max="-6"
+                      color="teal"
+                    />
+                  </q-item-section>
+                  <q-item-section side>
+                    <q-btn
+                      @click="settingsOpen = true"
+                      :ripple="false"
+                      round
+                      icon="settings"
+                      class="open-settings"
+                      size="sm"
+                    />
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </div>
+          </q-card-section>
+        </q-card>
+      </q-flashcard-section>
+
+      <q-flashcard-section transition="slide-up-in" :active="settingsOpen">
+        <q-card class="synth-panel-back" flat bordered>
+          <q-btn
+            @click="settingsOpen = false"
+            :ripple="false"
+            round
+            icon="close"
+            class="close-settings"
+            size="sm"
+          />
+        </q-card>
+      </q-flashcard-section>
+    </div>
+  </q-flashcard>
 </template>
 
 <script>
@@ -251,6 +281,7 @@ export default {
   name: "Synth",
   props: ["name"],
   data: () => ({
+    settingsOpen: false,
     isActive: false,
 
     scaleIndex: 0,
@@ -328,11 +359,19 @@ $seq-color-inactive: change-color($seq-color, $alpha: 0.3);
 $note-color-inactive: change-color($note-color, $alpha: 0.3);
 $octave-color-inactive: change-color($octave-color, $alpha: 0.3);
 
-.synth-panel {
+.synth-panel-front {
   width: 100%;
   max-width: 500px;
   height: 325px;
   background-color: $grey-9;
+}
+
+.synth-panel-back {
+  width: 100%;
+  min-width: 500px;
+  max-width: 500px;
+  height: 325px;
+  background-color: $grey-3;
 }
 
 .third-button {
@@ -391,8 +430,13 @@ $octave-color-inactive: change-color($octave-color, $alpha: 0.3);
   background-color: $teal;
 }
 
-.settings {
+.open-settings {
   color: $grey-9;
   background-color: $grey-1;
+}
+
+.close-settings {
+  color: $grey-1;
+  background-color: $grey-9;
 }
 </style>
